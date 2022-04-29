46ae7e652d586f7a03a93660ec5eb90d54b7d504
ParvesPR <parves2585@gmail.com>
2022-04-29 13:08:05 +0600

    created api for send data to ui

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion } = require('mongodb');
@@ -10,20 +11,29 @@ app.use(express.json());
 
 // connect mongobd
 
-const uri = "mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jr74c.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jr74c.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  console.log('MONGO DB CONNECTED')
-  client.close();
-});
+console.log('Mongodb connected');
+async function run() {
+  try {
+    await client.connect();
+    const productCollection = client.db('amazondb').collection('products');
 
+    app.get('/product', async (req, res) => {
+      const query = {};
+      const cursor = productCollection.find(query);
+      const products = await cursor.toArray();
+      res.send(products)
+    });
+  }
+  finally { }
+};
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
-    res.send('Amazon server is running')
+  res.send('Amazon server is running')
 });
 
 app.listen(port, () => {
-    console.log('Amazon running to port', port)
+  console.log('Amazon running to port', port)
 })
